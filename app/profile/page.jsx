fddd496238f54c937d3e5bb40799828f561c647c
Polina <polina2001sokolovas@gmail.com>
2024-02-07 22:45:05 +0100

    one more time

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -38,7 +38,7 @@ const MyProfile = () => {
           method: "DELETE",
         });
 
-        const filteredPosts = posts.filter((item) => item._id !== post._id);
+        const filteredPosts = myPosts.filter((item) => item._id !== post._id);
 
         setMyPosts(filteredPosts);
       } catch (error) {
