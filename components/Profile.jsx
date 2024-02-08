82e1c264614fdae46cc5699652b6784cfd38032a
Polina <polina2001sokolovas@gmail.com>
2024-02-08 18:48:43 +0100

    f

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -4,7 +4,7 @@ const Profile = ({ name, desc, data, handleEdit, handleDelete}) => {
   return (
     <section className="w-full">
       <h1 className="head_text text-left">
-        <span className="blue_gradient">{name} Profile</span>
+        <span className="orange_gradient text-center">{name} Profile</span>
       </h1>
       <p className="desc text-left"></p>
       <div className="mt-16 prompt_layout">
